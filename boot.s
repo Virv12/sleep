d3936c51f99eb1ca4c5587c58bdbacae2f64aef6
Filippo Casarin <casarin.filippo17@gmail.com>
2021-01-24 00:03:50 +0100

    Generic boot.s syscalls with automatic sections garbage collection

--- boot.s
+++ boot.s
@@ -1,7 +1,7 @@
 .intel_syntax noprefix
-.text
-.globl _start, sys_write, sys_nanosleep
+.globl _start, syscall, sys_write, sys_nanosleep, sys_exit
 
+.text
 _start:
 	pop rdi
 	mov rsi, rsp
@@ -9,16 +9,30 @@ _start:
 	and rsp, -16
 	call main
 	mov rdi, rax
+
+sys_exit:
 	mov rax, 60
 	syscall
 
+.section .text.sys_write
 sys_write:
 	mov rax, 1
-	jmp syscall
+	syscall
+	ret
 
+.section .text.sys_nanosleep
 sys_nanosleep:
 	mov rax, 35
+	syscall
+	ret
 
+.section .text.syscall
 syscall:
+	mov rax, rdi
+	mov rdi, rsi
+	mov rsi, rdx
+	mov rdx, rcx
+	mov r10, r8
+	mov r8, r9
 	syscall
 	ret
